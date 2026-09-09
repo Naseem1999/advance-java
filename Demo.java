ce6caeb3a1068ca3828e7e77c1b309908aac3b71
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-09-09 23:09:01 +0500

    abstract and anonymous inner class

--- Demo.java
+++ Demo.java
@@ -1,22 +1,30 @@
-class A{
-    public void show(){
-        System.out.println("showing....");
-    }
+abstract class A{
+    public abstract void show();
 
 }
-class B extends A{
-    public void show(){
-        System.out.println("In B show");
-    }
-}
+// class B extends A{
+//     public void show(){
+//         System.out.println("In B show");
+//     }
+// }
 class Demo{
      public static void main(String[] args) {
       
         // A obj=new B();
-        A obj=new A()
-        {//inner class anonomous classS
+        // A obj=new A()
+        //{//inner class anonomous classS
+            // public void show(){
+            //     System.out.println("In new Show");
+            // }
+        // };
+        // obj.show();
+
+        // A obj =new B();
+        // obj.show();
+
+        A obj=new A() {
             public void show(){
-                System.out.println("In new Show");
+                System.out.println("In a new show");
             }
         };
         obj.show();
