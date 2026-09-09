f9c27f43992c557f804989aa4843f0a22d37ff0b
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-09-09 23:23:56 +0500

    interface in java

--- Demo.java
+++ Demo.java
@@ -1,32 +1,24 @@
-abstract class A{
-    public abstract void show();
-
+interface A{
+    int age=10 ;  //every variable inside interface is final and static
+    int area=23;
+    void show();
+    void config();
 }
-// class B extends A{
-//     public void show(){
-//         System.out.println("In B show");
-//     }
-// }
+class B implements A{
+   public void show(){
+     System.out.println("In show");
+   }
+   public void config(){
+     System.out.println("In config");
+   }
+}
 class Demo{
      public static void main(String[] args) {
-      
-        // A obj=new B();
-        // A obj=new A()
-        //{//inner class anonomous classS
-            // public void show(){
-            //     System.out.println("In new Show");
-            // }
-        // };
-        // obj.show();
-
-        // A obj =new B();
-        // obj.show();
-
-        A obj=new A() {
-            public void show(){
-                System.out.println("In a new show");
-            }
-        };
-        obj.show();
+       A obj;
+       obj=new B();
+       obj.show();
+       obj.config();
+    //    A.area=233;
+       System.out.println(A.area);
      }
 }
