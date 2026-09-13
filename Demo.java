f17b95e9880563e17c91def393ddc50713444f6c
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-09-13 22:39:14 +0500

    try with resources

--- Demo.java
+++ Demo.java
@@ -14,8 +14,36 @@ class Demo{
         //  System.out.println(num);
         //  bf.close();
 
-        Scanner sc=new Scanner(System.in);
-        int num=sc.nextInt();
-        System.out.println(num);
+        // Scanner sc=new Scanner(System.in);
+        // int num=sc.nextInt();
+        // System.out.println(num);
+
+        // int i=2;
+        // int j=0;
+        // try{
+        //     j=18/i;
+        //     System.out.println("Bye......");
+        // }catch(Exception e){
+        //     System.out.println("Something went wrong");
+        // }finally{
+        //     System.out.println("neeto..");
+        // }
+
+        int num=0;
+        // BufferedReader br=null;
+        // try{
+        //   InputStreamReader in=new InputStreamReader(System.in);
+        //   br=new BufferedReader(in);
+        //   num=Integer.parseInt(br.readLine());
+        //   System.out.println(num);
+        // }
+        // finally{
+        //   br.close();
+        // }
+
+        try (BufferedReader bf=new BufferedReader(new InputStreamReader(System.in))) {
+            num=Integer.parseInt(bf.readLine());
+            System.out.println(num);    
+        } 
      }
 }
