833d620b470c837640f63474c5935d3d72aa90d8
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-09-13 19:10:37 +0500

    throw keyword in java

--- Demo.java
+++ Demo.java
@@ -6,21 +6,19 @@ class Demo{
         // System.out.println(j);
         // System.out.println("Bye");
 
-        int i=2;
+        int i=20;
         int j=0;
-        String str=null;
-
-        int nums[]=new int[5];
+       
         try{
            j=18/i;
-           System.out.println(str.length());
-           System.out.println(nums[1]);
-           System.out.println(nums[5]);
+           if(j==0)
+              throw new ArithmeticException("I dont want to print zero");
+       
         }catch(ArithmeticException e){
-            System.out.println("Can not divided By zero");
-        }catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("Stay in your Limit");
-        }catch(Exception e){
+            j=18/1;
+            System.out.println("This the default output "+e);
+        }
+        catch(Exception e){
             System.out.println("Something went wrong.. "+e);
         }
         System.out.println(j);
