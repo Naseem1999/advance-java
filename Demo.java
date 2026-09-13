d47b3f491009a2abe9bc6659f378a40ba7b0780c
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-09-13 18:33:33 +0500

    different types of exceptions

--- Demo.java
+++ Demo.java
@@ -6,12 +6,22 @@ class Demo{
         // System.out.println(j);
         // System.out.println("Bye");
 
-        int i=0;
+        int i=2;
         int j=0;
+        String str=null;
+
+        int nums[]=new int[5];
         try{
            j=18/i;
+           System.out.println(str.length());
+           System.out.println(nums[1]);
+           System.out.println(nums[5]);
+        }catch(ArithmeticException e){
+            System.out.println("Can not divided By zero");
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Stay in your Limit");
         }catch(Exception e){
-            System.out.println("Something went wrong");
+            System.out.println("Something went wrong.. "+e);
         }
         System.out.println(j);
         System.out.println("Bye");
