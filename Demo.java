dddd8155720438d656979e7ab7e3d493cc431451
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-09-13 16:12:05 +0500

    Enum class in java

--- Demo.java
+++ Demo.java
@@ -1,5 +1,24 @@
-enum Status{
+enum Status{  //status By default exteneds from ENUM CLASS
     Running,Failed,Pending,Success;
+}
+
+enum Laptop{
+    MacBook(2000),Xps(2100),Surface,Hp(3230);
+
+    private int price;
+    
+    private Laptop() {
+        price=500;
+    }
+    private  Laptop(int price){
+          this.price=price;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
 class Demo{
      public static void main(String[] args) {
@@ -10,22 +29,23 @@ class Demo{
     //   for(Status s1 :s)
     //   System.out.println(s1 +": "+s1.ordinal());
 
-      Status s=Status.Running;
+    //   Status s=Status.Running;
+    //   System.out.println(s.getClass().getSuperclass());
 
-      switch (s) {
-        case Running:
-            System.out.println("All Good");
-            break;
-        case Failed:
-        System.out.println("Try Again");
-            break;
-        case Pending:
-        System.out.println("Please Wait");
-            break; 
-        default:
-            System.out.println("Done");
-            break;
-      }
+    //   switch (s) {
+    //     case Running:
+    //         System.out.println("All Good");
+    //         break;
+    //     case Failed:
+    //     System.out.println("Try Again");
+    //         break;
+    //     case Pending:
+    //     System.out.println("Please Wait");
+    //         break; 
+    //     default:
+    //         System.out.println("Done");
+    //         break;
+    //   }
 
 
     //   if(s==Status.Running)
@@ -36,5 +56,8 @@ class Demo{
     //     System.out.println("Please Wait");
     //   else
     //     System.out.println("Done");
+
+    for(Laptop l :Laptop.values())
+        System.out.println(l +" : " + l.getPrice());
      }
 }
