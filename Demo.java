fcf8eaa17cff1ff5f12ab538fb45c275314f5ac6
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-09-15 22:03:41 +0500

    controlling thread running statements in one by one

--- Demo.java
+++ Demo.java
@@ -2,6 +2,9 @@ class A extends Thread{
     public void run(){
         for(int i=0;i<=100;i++){
             System.out.println("Hi");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {e.printStackTrace();}
         }
     }
 }
@@ -9,6 +12,9 @@ class B extends Thread{
     public void run(){
         for(int i=0;i<=100;i++){
             System.out.println("Hello");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {e.printStackTrace();}
         }
     }
 }
@@ -18,7 +24,12 @@ class Demo{
      public static void main(String[] args) {
       A obj1=new A();
       B obj2=new B();
+    //   System.out.println(obj1.getPriority());
+    // obj2.setPriority(Thread.MAX_PRIORITY);
       obj1.start();
+      try {
+          Thread.sleep(2);
+         } catch (InterruptedException e) {e.printStackTrace();}
       obj2.start();
      }
 }
