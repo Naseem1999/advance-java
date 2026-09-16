ed6077bf24b3aabe9bcecdbc4571506e95ac85f2
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-09-16 19:57:41 +0500

    Race condition in java

--- Demo.java
+++ Demo.java
@@ -1,64 +1,47 @@
-// class A implements Runnable{
-//     public void run(){
-//         for(int i=0;i<=100;i++){
-//             System.out.println("Hi");
-//             try {
-//                 Thread.sleep(10);
-//             } catch (InterruptedException e) {e.printStackTrace();}
-//         }
-//     }
-// }
-class B implements Runnable{
-    public void run(){
-        for(int i=0;i<=100;i++){
-            System.out.println("Hello");
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {e.printStackTrace();}
-        }
+class Counter
+{
+	int count;
+	// public void increment()
+	public synchronized void increment()
+	{
+		count++;
+	}
+}
+
+public class Demo {
+    public static void main(String[] args) throws InterruptedException{   
+    	
+    	Counter c=new Counter();
+    	
+    	Runnable obj1=()->
+    	{
+   		for(int i=1;i<=1000;i++)
+    		// for(int i=1;i<=10000;i++)
+    		{
+    			c.increment();
+    		}
+    	};
+    	
+    	Runnable obj2=()->
+    	{
+   		for(int i=1;i<=1000;i++)
+    		// for(int i=1;i<=10000;i++)
+    		{
+    			c.increment();
+    		}
+    	};
+    	
+    	Thread t1=new Thread(obj1);
+    	Thread t2=new Thread(obj2);
+    	 	
+    	t1.start();
+    	t2.start();
+    	
+    	t1.join();
+    	t2.join();
+    	
+    	System.out.println(c.count);
     }
 }
 
-class Demo{
     
-     public static void main(String[] args) {
-    //   A obj1=new A();
-    //   B obj2=new B();
-
-        // Runnable obj1=new A();
-        // Runnable obj2=new B();
-        Runnable obj1=() ->{
-        for(int i=0;i<=10;i++){
-            System.out.println("Hello");
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {e.printStackTrace();}
-        }
-         };
-
-         Runnable obj2=() ->{
-        for(int i=0;i<=10;i++){
-            System.out.println("Hi");
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {e.printStackTrace();}
-        }
-         };
-        
-    //   System.out.println(obj1.getPriority());
-    // obj2.setPriority(Thread.MAX_PRIORITY);
-    //   obj1.start();
-    //   try {
-    //       Thread.sleep(2);
-    //      } catch (InterruptedException e) {e.printStackTrace();}
-    //   obj2.start();
-
-    Thread t1=new Thread(obj1);
-    Thread t2=new Thread(obj2);
-
-    t1.start();
-    t2.start();
-
-
-     }
-}
