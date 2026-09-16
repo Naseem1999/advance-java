04194a3863a80f86f2f04984d6a732595d6a6c06
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-09-16 22:31:38 +0500

    Map ,hashmap and hashtable

--- Demo.java
+++ Demo.java
@@ -1,49 +1,27 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
 
 public class Demo {
     public static void main(String[] args) {   
-        Collection<Integer> nums=new TreeSet<Integer>();
-
-        // Collection<Integer> nums=new ArrayList<Integer>();  //no need to loop through the arraylist
-        // Collection nums=new ArrayList(); 
-        //if you want to working with indexes than you have to use List
-
-        // List<Integer> nums=new ArrayList<Integer>();
-        // Set<Integer> nums=new HashSet<Integer>(); //set is not following the sequence set does not support index value
-        //if you want sorting then use treeselect
-        // Set<Integer> nums=new TreeSet<Integer>();
+    //  Map<String,Integer> students=new HashMap<>();
+     Map<String,Integer> students=new Hashtable<>();
+     students.put("Navin",32);
+     students.put("Harsh",55);
+     students.put("Sushil",67);
+     students.put("Kiran",565);
+     students.put("Harsh",23);
 
 
-        nums.add(62);
-        nums.add(54);
-        nums.add(84);
-        nums.add(24);
-        nums.add(63);
-        
-        
-        // System.out.println(nums.get(2));
-        // System.out.println(nums.indexOf(6));
-        // nums.add("dasda");
-        
-        // for(int n :nums){
-            // for(Object n :nums){
-                //     int num=(Integer)n;
-                //    System.out.println(num*2);
-                // }
-                
-        Iterator<Integer> values=nums.iterator();
-        while(values.hasNext())
-               System.out.println(values.next());
-        // for(int n:nums){
-        //     System.out.println(n);
-        // }
-        // System.out.println(nums);  
+     System.out.println(students);
+     System.out.println(students.get("Navin"));
+     System.out.println(students.keySet());
+
+
+     for(String key:students.keySet()){
+        System.out.println(key +" : "+ students.get(key));
+     }
+
     }
 }
 
