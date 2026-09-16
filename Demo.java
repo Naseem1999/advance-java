9a6cebd71d2f3075c2fa370e03579a039bada346
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-09-16 21:36:15 +0500

    Collection and array list in java

--- Demo.java
+++ Demo.java
@@ -1,46 +1,30 @@
-class Counter
-{
-	int count;
-	// public void increment()
-	public synchronized void increment()
-	{
-		count++;
-	}
-}
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class Demo {
-    public static void main(String[] args) throws InterruptedException{   
-    	
-    	Counter c=new Counter();
-    	
-    	Runnable obj1=()->
-    	{
-   		for(int i=1;i<=1000;i++)
-    		// for(int i=1;i<=10000;i++)
-    		{
-    			c.increment();
-    		}
-    	};
-    	
-    	Runnable obj2=()->
-    	{
-   		for(int i=1;i<=1000;i++)
-    		// for(int i=1;i<=10000;i++)
-    		{
-    			c.increment();
-    		}
-    	};
-    	
-    	Thread t1=new Thread(obj1);
-    	Thread t2=new Thread(obj2);
-    	 	
-    	t1.start();
-    	t2.start();
-    	
-    	t1.join();
-    	t2.join();
-    	
-    	System.out.println(c.count);
+    public static void main(String[] args) {   
+
+        // Collection<Integer> nums=new ArrayList<Integer>();  //no need to loop through the arraylist
+        // Collection nums=new ArrayList(); 
+        //if you want to working with indexes than you have to use List
+
+        List<Integer> nums=new ArrayList<Integer>();
+        nums.add(6);
+        nums.add(5);
+        nums.add(8);
+        nums.add(2);
+
+        System.out.println(nums.get(2));
+        System.out.println(nums.indexOf(5));
+        // nums.add("dasda");
+        
+        // for(int n :nums){
+        // for(Object n :nums){
+            // int num=(Integer)n;
+        //    System.out.println(n);
+        // }
+        // System.out.println(nums);  
     }
 }
 
