ad42bd2277f50ee88d4e335d66daf564a60d57ef
Muhammad Naseem <naseemmuhammad700@gmail.com>
2026-09-17 22:26:56 +0500

    new date and time api in java

--- Demo.java
+++ Demo.java
@@ -1,13 +1,32 @@
-import java.util.Date;
+import java.time.*;
 
 public class Demo {
    public static void main(String[] args) {
-     Date date=new Date();
-     long time=date.getTime();
-     System.out.println(time);
+    //  Date date=new Date();
+    //  long time=date.getTime();
+    //  System.out.println(time);
 
-     java.sql.Date sdate=new  java.sql.Date(time);
-     System.out.println(sdate);
+    //  java.sql.Date sdate=new  java.sql.Date(time);
+    //  System.out.println(sdate);
+
+    LocalDate date=LocalDate.now();
+    LocalTime localTime=LocalTime.now();
+
+    int dayOfMonth=date.getDayOfMonth();
+    System.out.println(dayOfMonth);
+
+    int dayOfYear=date.getDayOfYear();
+    System.out.println(dayOfYear);
+    
+    int month=date.getMonthValue();
+    int year=date.getYear();
+
+
+    System.out.println(dayOfMonth+ " / " +month+ " / " +year);
+
+
+    System.out.println(date);
+    System.out.println(localTime);
    }
 
     
